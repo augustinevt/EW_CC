72307393c5b8328abc899be3dc85da0f80c085ea
august von trapp <augustvontrapp@gmail.com>
2019-12-03 00:46:45 -0800

    setup project sketch API call

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,8 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi': 0,
+    'no-console': 0,
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   parserOptions: {

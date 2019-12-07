94dfd9e65c4e0d227cbc3a2d3d1dca5df4cfc782
august von trapp <augustvontrapp@gmail.com>
2019-12-07 01:49:28 -0800

    remove api keys and add proxy init script

--- src/api.js
+++ src/api.js
@@ -1,16 +1,7 @@
 export default async ({
   section, sort, resultWindow, viral,
 }) => {
-  const res = await fetch(
-    `http://localhost:3000?section=${section}&sort=${sort}&resultWindow=${resultWindow}&viral=${viral}`,
-    {
-      headers: new Headers(
-        {
-          Authorization: 'Client-ID 3ed4c0157f3d743',
-        },
-      ),
-    },
-  )
+  const res = await fetch(`http://localhost:3000?section=${section}&sort=${sort}&resultWindow=${resultWindow}&viral=${viral}`)
 
   const parsedData = await res.json()
 
